5f1861f64675a3033347e68d0c6bb8397cc96026
yyr <2812154848@qq.com>
2018-12-04 13:26:25 +0800

    启动类注解修改

--- xingtong/src/main/java/com/xt/xingtong/App.java
+++ xingtong/src/main/java/com/xt/xingtong/App.java
@@ -3,11 +3,19 @@ package com.xt.xingtong;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Hello world!
  *
  */
+
+@EnableSwagger2
+@EnableTransactionManagement
+@EnableScheduling
 @SpringBootApplication
 @MapperScan("com.xt.xingtong.interf")//扫描dao接口
 public class App 
